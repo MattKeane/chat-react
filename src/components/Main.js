86cfbb013d7bd24386283d75fc34863eed17ad42
Matt Keane <mattpkeane@gmail.com>
2021-05-31 15:17:22 -0500

    added NavBar component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,15 @@
-import { useContext } from 'react';
+import React, { useContext } from 'react';
 import { UserContext } from '../App';
+import NavBar from './NavBar';
 
 export default function Main(props) {
 
 	const currentUser = useContext(UserContext);
 
 	return (
-		<p>Welcome, {currentUser.username}!</p>
+		<React.Fragment>
+			<NavBar />
+			<p>Welcome, {currentUser.username}!</p>
+		</React.Fragment>
 	)
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+export default function NavBar(props) {
+	return (
+		<nav>
+			<ul>
+				<li className="pseudolink">Log Out</li>
+			</ul>
+		</nav>
+	)
+}
